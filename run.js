561cf4aacab4db3e81a11ab6c96886499eec358b
Kay Lovelace <kay@makersacademy.com>
2018-03-08 10:47:19 +0000

    Send message to the channel

--- run.js
+++ run.js
@@ -1,12 +1,15 @@
-const CONFIG = require('./env.json');
+const CONFIG = require("./env.json");
 const AIRTABLE_API_KEY = CONFIG.AIRTABLE_API_KEY;
-const BASE_ID = CONFIG.BASE_ID;
+const AIRTABLE_BASE_ID = CONFIG.AIRTABLE_BASE_ID;
+const SLACK_WEBHOOK_URL = CONFIG.SLACK_WEBHOOK_URL;
+const { IncomingWebhook, WebClient } = require("@slack/client");
+const Airtable = require("airtable");
+const dateformat = require("dateformat");
 
-var Airtable = require("airtable");
 Airtable.configure({
   apiKey: AIRTABLE_API_KEY
 });
-var base = Airtable.base(BASE_ID);
+var base = Airtable.base(AIRTABLE_BASE_ID);
 
 const ALL_FORMULA = "1";
 const TODAY_FORMULA = "IS_SAME({End time}, TODAY(), 'day')";
@@ -35,13 +38,66 @@ function getTasks(viewName, formula = ALL_FORMULA) {
   });
 }
 
-function formatCoachesList(coaches) {
-  return coaches.map(coach => coach.name).join(", ");
+function formatCoachesList(coaches, handleLookup) {
+  return coaches
+    .map(coach => {
+      return `${coach.name} (<@${handleLookup(coach.id)}>)`;
+    })
+    .join(", ");
 }
 
-getTasks("Upcoming", TODAY_FORMULA).then(records => {
-  records.forEach(record => {
-    const coaches = formatCoachesList(record.get("Coaches"))
-    console.log("Today:", coaches, "runs", record.get("Name"))
-  })
-}).catch(err => console.error(err))
+function formatStartTime(dateString) {
+  const date = new Date(dateString);
+  return dateformat(date, "HH.MM");
+}
+
+function createCoachingDailyUpdateMessage(handleLookup) {
+  return getTasks("Upcoming", TODAY_FORMULA)
+    .then(records => {
+      return (
+        "Today's tasks:\n" +
+        records
+          .map(record => {
+            const coaches = formatCoachesList(
+              record.get("Coaches"),
+              handleLookup
+            );
+            const startFormatted = formatStartTime(record.get("Start time"));
+            return `${startFormatted}: ${coaches} runs ${record.get("Name")}`;
+          })
+          .join("\n")
+      );
+    })
+    .catch(err => console.error(err));
+}
+
+function makeSlackHandleLookup() {
+  return new Promise((resolve, reject) => {
+    base("Coaches")
+      .select({
+        view: "Grid view"
+      })
+      .firstPage((err, records) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(records);
+      });
+  }).then(records => {
+    return id =>
+      records.find(record => record.get("User").id === id).get("Slack ID");
+  });
+}
+
+makeSlackHandleLookup().then(lookup => {
+  createCoachingDailyUpdateMessage(lookup).then(message => {
+    const dailyUpdateHook = new IncomingWebhook(SLACK_WEBHOOK_URL);
+    dailyUpdateHook.send(message, (error, resp) => {
+      if (error) {
+        return console.error(error);
+      }
+      console.log("Notification sent");
+    });
+  });
+});
