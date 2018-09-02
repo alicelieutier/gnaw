d344e7e6ba62fdce60abb1a708d0cd0583eb750c
Kay Lovelace <kay@makersacademy.com>
2018-09-02 21:49:19 +0100

    Break out aggregated generators in prep for apprs

--- commands/reminder-clock.js
+++ commands/reminder-clock.js
@@ -1,65 +1,29 @@
 import { WebClient } from "@slack/client";
 import moment from "moment-timezone";
-import getTasks from "../lib/getTasks";
-import base from "../lib/base";
-import StudentReminderGenerator from "../lib/message_generators/StudentReminderGenerator";
-import ArbitraryMessageGenerator from "../lib/message_generators/ArbitraryMessageGenerator";
-import WeekdailyGenerator from "../lib/message_generators/WeekdailyGenerator";
-import makeSlackHandleLookup from "../lib/makeSlackHandleLookup";
-import makeCoachSummary from "../lib/makeCoachSummary";
-import Message from "../lib/Message";
+import AcademyStudentGenerators from "../lib/generators/AcademyStudentGenerators";
+import AcademyStaffGenerators from "../lib/generators/AcademyStaffGenerators";
+import AcademyBase from "../lib/bases/AcademyBase";
+import ApprenticeshipsBase from "../lib/bases/ApprenticeshipsBase";
+
 const SLACK_TOKEN_STAFF = process.env.SLACK_TOKEN_STAFF;
 const SLACK_TOKEN_STUDENT = process.env.SLACK_TOKEN_STUDENT;
 
 const staffSlackClient = new WebClient(SLACK_TOKEN_STAFF);
 const studentSlackClient = new WebClient(SLACK_TOKEN_STUDENT);
 
-function makeCohortChannelLookup() {
-  const recordsPromise = base("Cohorts")
-    .select({ view: "Grid view" })
-    .firstPage();
-  return async id => {
-    const records = await recordsPromise;
-    return records.find(record => record.id === id).get("Slack channel");
-  };
-}
-
-const cohortChannelLookup = makeCohortChannelLookup();
-
-const STUDENT_GENERATORS = [
-  new StudentReminderGenerator(
-    formula => getTasks(base, "Upcoming", formula),
-    cohortChannelLookup,
-    recordId => base("Tasks").update(recordId, { "Reminder given?": true })
-  ),
-  new ArbitraryMessageGenerator(
-    formula =>
-      base("Messages")
-        .select({ view: "Upcoming", filterByFormula: formula })
-        .firstPage(),
-    cohortChannelLookup,
-    recordId => base("Messages").update(recordId, { "Sent?": true })
-  )
-];
-
-const STAFF_GENERATORS = [
-  new WeekdailyGenerator(9, 0, async (timestamp, id) => {
-    const handleLookup = await makeSlackHandleLookup(base);
-    const message = await makeCoachSummary(
-      timestamp,
-      (view, formula) => getTasks(base, view, formula),
-      handleLookup
-    );
-    return [new Message(message, "#coaches", id)];
-  })
-];
+const ACADEMY_STUDENT_GENERATORS = AcademyStudentGenerators(AcademyBase);
+const ACADEMY_STAFF_GENERATORS = AcademyStaffGenerators(AcademyBase);
 
 async function reminderClock() {
   const now = moment().tz("Europe/London");
   const currentTimestamp = now.valueOf();
   console.log("Polling reminders for", now.format());
-  await sendReminders(currentTimestamp, STUDENT_GENERATORS, studentSlackClient);
-  await sendReminders(currentTimestamp, STAFF_GENERATORS, staffSlackClient);
+  await sendReminders(currentTimestamp, ACADEMY_STUDENT_GENERATORS, studentSlackClient).catch(
+    console.error
+  );
+  await sendReminders(currentTimestamp, ACADEMY_STAFF_GENERATORS, staffSlackClient).catch(
+    console.error
+  );
   setTimeout(reminderClock, 60 * 1000);
 }
 
